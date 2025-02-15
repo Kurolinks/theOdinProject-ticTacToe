d4fcca79773b8656ef7c5d11123f71f494a8741d
Woyengikuro Subiri <wsubiri@gmail.com>
2025-02-15 13:38:04 +0100

    check dondition for win

--- index.js
+++ index.js
@@ -31,11 +31,12 @@
 
 let board = [];
 let counter = 1;
+let markerChoice;
 
 for (let i = 0; i < 3; i++) {
 board[i] = [];
     for (let j = 0; j < 3; j++) {
-        board[i][j] = counter++;
+        board[i][j] = counter;
     }
 }
 
@@ -44,74 +45,96 @@ var computer;
 
 
 function playerTurn() {
-    let markerChoice = prompt('Pick a Marker: X or O').toLowerCase;
+    markerChoice = prompt('Pick a Marker: X or O');
     let firstIndex = parseInt(prompt("Pick a number from 0 to 2 to select the Row"), 10);
     let secondIndex = parseInt(prompt("Pick a number from 0 to 2 to select the Column"), 10);
 
-    if ((markerChoice === "A")) {
-        board[firstIndex][secondIndex] = markerChoice;
+    if ((markerChoice === "o")) {
+        if (board[firstIndex][secondIndex] === 1) {
+            board[firstIndex][secondIndex] = markerChoice;
+        }
+        else {
+            alert("Invalid");
+        }
     }
     else if ((markerChoice === "x")) {
-        board[firstIndex][secondIndex] = markerChoice;
+        if (board[firstIndex][secondIndex] === 1) {
+            board[firstIndex][secondIndex] = markerChoice;
+        }
     }
     console.log(board);
 }
-// if (positionChoice == 1) {
-//     console.log("X")
-// }
+
 
 function computerTurn() {
-    let options = ['X', 'O'];
-    let index = Math.floor(Math.random() * 2);
+    let option;
     let compSecondIndex = Math.floor(Math.random() * 2);
     let compFirstIndex = Math.floor(Math.random() * 2);
-    let computerChoice = options[index];
+
+    if (playerTurn === "x") {
+        option = "o";
+        let computerChoice = option;
+        if (board[compFirstIndex][compSecondIndex] === 1) {
+            board[compSecondIndex][compSecondIndex] = computerChoice;
+        }
+    }
+    else if (markerChoice === "o") {
+        option = "x";
+        let computerChoice = option;
+        if (board[compFirstIndex][compSecondIndex] === 1) {
+            board[compSecondIndex][compSecondIndex] = computerChoice;
+        }
+    }
     // console.log(computerChoice);
     // console.log(compFirstIndex);
     // console.log(compSecondIndex);
-
-    if ((computerChoice.toLocaleLowerCase === "o")) {
-        board[compSecondIndex][compSecondIndex] = computerChoice;
-    }
-    else if ((computerChoice.toLowerCase = 'x')) {
-        board[compSecondIndex][compSecondIndex] = computerChoice;
-    }
 }
 
 
 function winningCondition() {
-    if ((board[0] === board[1]) && (board[1] === board[2])) {
+    if ((board[0][0] !== 1) && (board[0][0] === board[0][1]) && (board[0][1] === board[0][2])) {
         console.log("Game Over! 1");
     }
-    // else if ((board[3] === board[4]) && (board[4] === board[5])) {
-    //     console.log("Game Over! 2");
-    // }
-    // else if ((board[6] === board[7]) && (board[7] === board[8])) {
-    //     console.log("Game Over! 3");
-    // }
-    else if ((board[0] === board[3]) && (board[3] === board[6])) {
+    else if ((board[1][0] !== 1) && (board[1][0] === board[1][1]) && (board[1][1] === board[1][2])) {
+        console.log("Game Over! 2");
+    }
+    else if ((board[2][0] !== 1) &&(board[2][0] === board[2][1]) && (board[2][1] === board[2][2])) {
+        console.log("Game Over! 3");
+    }
+    else if ((board[0][0] !== 1) && (board[0][0] === board[1][0]) && (board[1][0] === board[2][0])) {
         console.log("Game Over! 4");
     }
-    else if ((board[1] === board[4]) && (board[4] === board[7])) {
+    else if ((board[0][1] !== 1) && (board[0][1] === board[1][1]) && (board[1][1] === board[2][1])) {
         console.log("Game Over! 5");
     }
-    else if ((board[2] === board[5]) && (board[5] === board[8])) {
+    else if ((board[0][2] !== 1) &&(board[0][2] === board[1][2]) && (board[1][2] === board[2][2])) {
         console.log("Game Over! 6");
     }
-    else if ((board[0] === board[4]) && (board[4] === board[8])) {
+    else if ((board[0][0] !== 1) && (board[0][0] === board[1][1]) && (board[1][1] === board[2][2])) {
         console.log("Game Over! 7");
     }
-    else if ((board[2] === board[4]) && (board[4] === board[6])) {
+    else if ((board[0][2] !== 1) &&(board[0][2] === board[1][1]) && (board[1][1] === board[2][0])) {
         console.log("Game Over! 8");
     }
     else {
-        // for(let i = 0; i < 9; i++){
-        //     if ((board[i] !== "o") || (board[i] !== "x")){
-                playerTurn();
-                computerTurn();
+        // for(let i = 0; i < 3; i++){
+        //     for(let j = 0; j < 3; j++) {
+        //         if ((board[i][j] !== "o") || (board[i][j] !== "x")){
+        //         }
         //     }
         // }
+        playerTurn();
+        computerTurn();
+        winningCondition();
     }
 }
 
 winningCondition();
+
+
+
+// Tod list
+// change 1 to -
+// alert message when trying to overwrite
+// Check who won and show it
+// cheeck for draw
