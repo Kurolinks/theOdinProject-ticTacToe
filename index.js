734fc434c4ab9885c15e5510f0572f115b34448f
Woyengikuro Subiri <wsubiri@gmail.com>
2025-02-17 15:38:06 +0100

    fix computer override

--- index.js
+++ index.js
@@ -30,7 +30,7 @@
 // set win conditions
 
 let board = [];
-let counter = 1;
+let counter = "-";
 let markerChoice;
 
 for (let i = 0; i < 3; i++) {
@@ -43,77 +43,105 @@ board[i] = [];
 var player;
 var computer;
 
-
+// Function for Player
 function playerTurn() {
     markerChoice = prompt('Pick a Marker: X or O');
     let firstIndex = parseInt(prompt("Pick a number from 0 to 2 to select the Row"), 10);
     let secondIndex = parseInt(prompt("Pick a number from 0 to 2 to select the Column"), 10);
 
     if ((markerChoice === "o")) {
-        if (board[firstIndex][secondIndex] === 1) {
+        if (board[firstIndex][secondIndex] === "-") {
             board[firstIndex][secondIndex] = markerChoice;
         }
         else {
-            alert("Invalid");
+            alert("You can't place a marker here, Play Again!");
+            playerTurn();
         }
     }
     else if ((markerChoice === "x")) {
-        if (board[firstIndex][secondIndex] === 1) {
+        if (board[firstIndex][secondIndex] === "-") {
             board[firstIndex][secondIndex] = markerChoice;
         }
+        else {
+            alert("You can't place a marker here, Play Again!");
+            playerTurn();
+        }
     }
-    console.log(board);
 }
 
-
+// Function for Computer 
 function computerTurn() {
     let option;
-    let compSecondIndex = Math.floor(Math.random() * 2);
-    let compFirstIndex = Math.floor(Math.random() * 2);
-
-    if (playerTurn === "x") {
+    // create random number from 0 to 2
+    let compSecondIndex = Math.floor(Math.random() * 3);
+    let compFirstIndex = Math.floor(Math.random() * 3);
+    if (markerChoice === "x") {
         option = "o";
         let computerChoice = option;
-        if (board[compFirstIndex][compSecondIndex] === 1) {
-            board[compSecondIndex][compSecondIndex] = computerChoice;
+
+        while (board[compFirstIndex][compSecondIndex] !== "-") {
+            compFirstIndex = Math.floor(Math.random() * 3);
+            compSecondIndex = Math.floor(Math.random() * 3);
         }
+
+        board[compFirstIndex][compSecondIndex] = computerChoice;
     }
+
+    // foremer code
+        // if (board[compFirstIndex][compSecondIndex] === "-") {
+        //     board[compFirstIndex][compSecondIndex] = computerChoice;
+        // }else {
+        //     alert("You can't place a marker here, Play Again!");
+        //     computerTurn();
+        // }
+    // }
     else if (markerChoice === "o") {
-        option = "x";
+        option = "x";   
+
         let computerChoice = option;
-        if (board[compFirstIndex][compSecondIndex] === 1) {
-            board[compSecondIndex][compSecondIndex] = computerChoice;
-        }
-    }
-    // console.log(computerChoice);
-    // console.log(compFirstIndex);
-    // console.log(compSecondIndex);
+
+        while (board[compFirstIndex][compSecondIndex] !== "-") {
+                    compFirstIndex = Math.floor(Math.random() * 3);
+                    compSecondIndex = Math.floor(Math.random() * 3);
+                }
+        
+                board[compFirstIndex][compSecondIndex] = computerChoice;
+            }
+
+        // former code
+        // if (board[compFirstIndex][compSecondIndex] === "-") {
+        //     board[compFirstIndex][compSecondIndex] = computerChoice;
+        // }else {
+        //     alert("You can't place a marker here, Play Again!");
+        //     computerTurn();
+        // }
+    // }
 }
 
-
+// Condition for winning
 function winningCondition() {
-    if ((board[0][0] !== 1) && (board[0][0] === board[0][1]) && (board[0][1] === board[0][2])) {
+    if ((board[0][0] !== "-") && (board[0][0] === board[0][1]) && (board[0][1] === board[0][2])) {
         console.log("Game Over! 1");
     }
-    else if ((board[1][0] !== 1) && (board[1][0] === board[1][1]) && (board[1][1] === board[1][2])) {
+    else if ((board[1][0] !== "-") && (board[1][0] === board[1][1]) && (board[1][1] === board[1][2])) {
         console.log("Game Over! 2");
     }
-    else if ((board[2][0] !== 1) &&(board[2][0] === board[2][1]) && (board[2][1] === board[2][2])) {
+    else if ((board[2][0] !== "-") &&(board[2][0] === board[2][1]) && (board[2][1] === board[2][2])) {
         console.log("Game Over! 3");
     }
-    else if ((board[0][0] !== 1) && (board[0][0] === board[1][0]) && (board[1][0] === board[2][0])) {
+    else if ((board[0][0] !== "-") && (board[0][0] === board[1][0]) && (board[1][0] === board[2][0])) {
         console.log("Game Over! 4");
     }
-    else if ((board[0][1] !== 1) && (board[0][1] === board[1][1]) && (board[1][1] === board[2][1])) {
+    else if ((board[0][1] !== "-") && (board[0][1] === board[1][1]) && (board[1][1] === board[2][1])) {
         console.log("Game Over! 5");
     }
-    else if ((board[0][2] !== 1) &&(board[0][2] === board[1][2]) && (board[1][2] === board[2][2])) {
+    else if ((board[0][2] !== "-") &&(board[0][2] === board[1][2]) && (board[1][2] === board[2][2])) {
         console.log("Game Over! 6");
     }
-    else if ((board[0][0] !== 1) && (board[0][0] === board[1][1]) && (board[1][1] === board[2][2])) {
+    else if ((board[0][0] !== "-") && (board[0][0] === board[1][1]) && (board[1][1] === board[2][2])) {
         console.log("Game Over! 7");
     }
-    else if ((board[0][2] !== 1) &&(board[0][2] === board[1][1]) && (board[1][1] === board[2][0])) {
+    else if ((board[0][2] !== "-") &&(board[0][2] === board[1][1]) && (board[1][1] === board[2][0])) {
         console.log("Game Over! 8");
     }
     else {
@@ -124,8 +152,11 @@ function winningCondition() {
         //     }
         // }
         playerTurn();
+        alert(board);
         computerTurn();
+        alert(board);
         winningCondition();
+        console.log(board);
     }
 }
 
@@ -133,8 +164,9 @@ winningCondition();
 
 
 
-// Tod list
-// change 1 to -
+// Todo list
 // alert message when trying to overwrite
 // Check who won and show it
 // cheeck for draw
+// ensure users second turn doesn'nt overwrite
+// stop computer from playing if game is won
